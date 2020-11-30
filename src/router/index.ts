c15c0410c891b004f26757c2f40958b13b1efbca
EDSON MATEUS ALVES DE OLIVEIRA <tioed2015@gmail.com>
2020-11-30 10:24:28 -0300

    routes config

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,12 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
-
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: "/home",
-    component: Home,
+    name: "Home",
+    component: () => import("../views/Home.vue"),
     children: [
       {
         path: "/createTaks",
