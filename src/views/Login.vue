b41cc49c4d693eff5ef6de61ca86e95ffdf73b20
EDSON MATEUS ALVES DE OLIVEIRA <tioed2015@gmail.com>
2020-12-01 12:50:04 -0300

    centralizar form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,31 +1,35 @@
 <template>
   <v-container>
-    <v-form v-model="valid">
-      <v-container>
-        <v-text-field
-          v-model="firstName"
-          :rules="nameRules"
-          :counter="10"
-          label="First name"
-          required
-        ></v-text-field>
+    <v-row>
+      <v-col align="center">
+        <v-form v-model="valid">
+          <v-container>
+            <v-text-field
+              v-model="firstName"
+              :rules="nameRules"
+              :counter="10"
+              label="First name"
+              required
+            ></v-text-field>
 
-        <v-text-field
-          v-model="lastName"
-          :rules="nameRules"
-          :counter="10"
-          label="Last name"
-          required
-        ></v-text-field>
+            <v-text-field
+              v-model="lastName"
+              :rules="nameRules"
+              :counter="10"
+              label="Last name"
+              required
+            ></v-text-field>
 
-        <v-text-field
-          v-model="email"
-          :rules="emailRules"
-          label="E-mail"
-          required
-        ></v-text-field>
-      </v-container>
-    </v-form>
+            <v-text-field
+              v-model="email"
+              :rules="emailRules"
+              label="E-mail"
+              required
+            ></v-text-field>
+          </v-container>
+        </v-form>
+      </v-col>
+    </v-row>
     <router-link to="/home/listTaks">home </router-link>
   </v-container>
 </template>
