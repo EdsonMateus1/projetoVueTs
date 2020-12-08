29a76c213e0a8d78e3a0a27b9d568fd2b05d9e93
EDSON MATEUS ALVES DE OLIVEIRA <tioed2015@gmail.com>
2020-12-08 12:10:44 -0300

    variaves de ambiente firebase

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,16 +1,6 @@
 <template>
   <v-container>
-    <v-form v-model="valid">
-      <v-text-field
-        class="margin"
-        v-model="firstName"
-        :rules="nameRules"
-        :counter="10"
-        label="First name"
-        required
-        type="text"
-      ></v-text-field>
-
+    <v-form @submit.prevent="doLogin" v-model="valid">
       <v-text-field
         class="margin"
         v-model="email"
@@ -42,22 +32,25 @@ import Button from "@/components/shared/button/Button.vue";
 })
 export default class Login extends Vue {
   private valid = false;
-  private firstName = "";
   private email = "";
   private password = "";
+  private passwordRules = [(v: any) => !!v || "password is required"];
 
-  private passwordRules = [
-    (v: any) => !!v || "password is required",
-    //(v: any) => v.length <= 10 || "Name must be less than 10 characters",
-  ];
-  private nameRules = [
-    (v: any) => !!v || "Name is required",
-    (v: any) => v.length <= 10 || "Name must be less than 10 characters",
-  ];
   private emailRules = [
     (v: any) => !!v || "E-mail is required",
     (v: any) => /.+@.+/.test(v) || "E-mail must be valid",
   ];
+
+  async doLogin() {
+    try {
+      const res = await this.$firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 </script>
 
