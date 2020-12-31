f2ae36b6477825fd68cef2cf5ff023cc40d2601c
EDSON MATEUS ALVES DE OLIVEIRA <tioed2015@gmail.com>
2020-12-31 14:19:56 -0300

    todo card

--- src/components/listTaks/ListTaks.vue
+++ src/components/listTaks/ListTaks.vue
@@ -1,55 +1,20 @@
 <template>
   <v-container class="main">
-    <v-row>
-      <v-card
-        class="ma-8"
-        min-width="350"
-        max-height="150"
-        color="#385F73"
-        dark
-      >
-        <v-card-title class="headline"> ir na academia </v-card-title>
-
-        <v-card-subtitle>tenho que ir malhar o bumbu</v-card-subtitle>
-
-        <v-card-actions>
-          <v-checkbox
-            v-model="checkbox"
-            label="Marca como completa"
-          ></v-checkbox>
-        </v-card-actions>
-      </v-card>
-
-      <v-card
-        class="ma-8"
-        min-width="350"
-        max-height="150"
-        color="#385F73"
-        dark
-      >
-        <v-card-title class="headline"> ir na academia </v-card-title>
-
-        <v-card-subtitle>tenho que ir malhar o bumbu</v-card-subtitle>
-
-        <v-card-actions>
-          <v-checkbox
-            v-model="checkbox"
-            label="Marca como completa"
-          ></v-checkbox>
-        </v-card-actions>
-      </v-card>
-    </v-row>
+    <todo-card></todo-card>
   </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import UserRepository from "@/repositories/user/userRepository";
+import TodoCard from "./components/TodosCard.vue";
 import { Todo } from "@/settings/types/userTypes";
-@Component
+
+@Component({
+  components: { TodoCard }
+})
 export default class CreateTaks extends Vue {
   private todoName = "comprar pao";
-  private checkbox = false;
   private completed = false;
   private todos: Array<Todo> | undefined;
   private userRepository = new UserRepository();
---
+++ src/components/listTaks/components/TodosCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-row>
+    <v-card class="ma-8" min-width="350" max-height="150" color="#385F73" dark>
+      <v-card-title class="headline"> ir na academia </v-card-title>
+
+      <v-card-subtitle>tenho que ir malhar o bumbu</v-card-subtitle>
+
+      <v-card-actions>
+        <v-checkbox v-model="checkbox" label="Marca como completa"></v-checkbox>
+      </v-card-actions>
+    </v-card>
+
+    <v-card class="ma-8" min-width="350" max-height="150" color="#385F73" dark>
+      <v-card-title class="headline"> ir na academia </v-card-title>
+
+      <v-card-subtitle>tenho que ir malhar o bumbu</v-card-subtitle>
+
+      <v-card-actions>
+        <v-checkbox v-model="checkbox" label="Marca como completa"></v-checkbox>
+      </v-card-actions>
+    </v-card>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class TodosCard extends Vue {
+  private checkbox = false;
+}
+</script>
+
+<style scoped></style>
